4e4566f475ff482c6ce1d1554348761924c5e1e1
ipekgoktan <igoktan20@windwardschool.org>
2019-07-10 19:41:18 -0400

    improved rainbow lights

--- src/kuri_mi/src/kuri_light.py
+++ src/kuri_mi/src/kuri_light.py
@@ -1,22 +1,13 @@
 #!/usr/bin/env python
 import rospy
-import sys
-import termios, tty, os, time
+import termios, tty, os, time, sys, math
 #import mobile_base_driver.msg
 
 from std_msgs.msg import String
 from mobile_base_driver.msg import ChestLeds
 from mobile_base_driver.msg import Led
 
-def run():
-    pub = rospy.Publisher('/mobile_base/commands/chest_leds', ChestLeds, queue_size = 10)
-    rospy.init_node('kuri_light')
-    rate = rospy.Rate(100)
-    l = ChestLeds()
-
-    print("Light Program Started")
-
-    def getch():
+def getch():
         fd = sys.stdin.fileno()
         old_settings = termios.tcgetattr(fd)
         try:
@@ -27,8 +18,37 @@ def run():
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
 
+def rainbow(frequency1, frequency2, frequency3, phase1, phase2, phase3):
+    pub = rospy.Publisher('/mobile_base/commands/chest_leds', ChestLeds, queue_size = 10)
+    rospy.init_node('kuri_light')
+    rate = rospy.Rate(100)
+    l = ChestLeds()
+
+    print("kuri rainbow working")
+    center = 128;
+    width = 127;
+    length = 50;
+
+    for i in range(length):
+        for p in range(len(l.leds)):
+            l.leds[p].red = math.sin(frequency1*i + phase1) * width + center;
+            l.leds[p].green = math.sin(frequency2*i + phase2) * width + center;
+            l.leds[p].blue = math.sin(frequency3*i + phase3) * width + center;
+        pub.publish(l)
+        time.sleep(0.1)
+
+def run():
+    pub = rospy.Publisher('/mobile_base/commands/chest_leds', ChestLeds, queue_size = 10)
+    rospy.init_node('kuri_light')
+    rate = rospy.Rate(100)
+    l = ChestLeds()
+
+    print("Light Program Started")
+    for i in range(len(l.leds)):
+        l.leds[i].red = 255
+        l.leds[i].green = 255
+        l.leds[i].blue = 255
     button_delay = 0.2
-    rainbowStart = True
 
     while not rospy.is_shutdown():
         try:
@@ -62,39 +82,7 @@ def run():
                 time.sleep(button_delay)
 
             if(letter == "q"):
-                for i in range(len(l.leds)):
-                    l.leds[i].red = 255
-                    l.leds[i].green = 0
-                    l.leds[i].blue = 0
-
-                while(l.leds[i].red > 0):
-                    for i in range(len(l.leds)):
-                                l.leds[i].red = l.leds[i].red - 20
-                                l.leds[i].green = l.leds[i].green + 20
-                                time.sleep(0.1)
-                                
-                                if (letter == "c"):
-                                    print("Teleop ended.")
-                                    exit(0)
-                                pub.publish(l)
-                while(l.leds[i].green > 0):
-                    for i in range(len(l.leds)):
-                                l.leds[i].green = l.leds[i].green - 20
-                                l.leds[i].blue = l.leds[i].blue + 20
-                                time.sleep(0.1)
-                                if (letter == "c"):
-                                    print("Teleop ended.")
-                                    exit(0)
-                                pub.publish(l)
-                while(l.leds[i].blue > 0):
-                    for i in range(len(l.leds)):
-                                l.leds[i].blue = l.leds[i].blue - 20
-                                l.leds[i].red = l.leds[i].red + 20
-                                time.sleep(0.1)
-                                if (letter == "c"):
-                                    print("Teleop ended.")
-                                    exit(0)
-                                pub.publish(l)
+                rainbow(.3, .3, .3, 0, 2, 4)
 
 
             if (letter == "c"):
