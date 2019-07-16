866c5701759e6a975ce5933a309588ea3d137d49
ipekgoktan <igoktan20@windwardschool.org>
2019-07-16 16:44:48 -0400

    follows faces and turns orange when touched

--- src/kuri_mi/src/kuri_face.py
+++ src/kuri_mi/src/kuri_face.py
@@ -6,7 +6,7 @@ import termios, tty, os, time, sys, math
 from std_msgs.msg import String
 from mobile_base_driver.msg import ChestLeds
 from mobile_base_driver.msg import Led
-from mobile_base_driver.msg import Touch
+
 #from geometry_msgs.msg import HeadClient
 #For face detection
 from vision_msgs.msg import FrameResults
@@ -21,9 +21,30 @@ from vision_msgs.msg import ImageQuality
 from vision_msgs.msg import ObjectFeatures
 #For body movement
 from geometry_msgs.msg import Twist
+from mobile_base_driver.msg import Touch
 
+face_detected = False
+touch_detected = False
 
-def light_up(frequency1, frequency2, frequency3, phase1, phase2, phase3):
+def happy():
+	pub = rospy.Publisher('/mobile_base/commands/chest_leds', ChestLeds, queue_size = 10)
+	l = ChestLeds()
+	for i in range(len(l.leds)):
+		l.leds[i].red = 200
+		l.leds[i].green = 50
+		l.leds[i].blue = 0
+	pub.publish(l)
+
+def white():
+	pub = rospy.Publisher('/mobile_base/commands/chest_leds', ChestLeds, queue_size = 10)
+	l = ChestLeds()
+	for i in range(len(l.leds)):
+		l.leds[i].red = 255
+		l.leds[i].green = 255
+		l.leds[i].blue = 255
+	pub.publish(l)
+
+def rainbow(frequency1, frequency2, frequency3, phase1, phase2, phase3):
 	pub = rospy.Publisher('/mobile_base/commands/chest_leds', ChestLeds, queue_size = 10)
 	#rospy.init_node('kuri_face')
 	#rate = rospy.Rate(100)
@@ -41,6 +62,10 @@ def light_up(frequency1, frequency2, frequency3, phase1, phase2, phase3):
 			l.leds[p].blue = math.sin(frequency3*i + phase3) * width + center;
 			pub.publish(l)
 		time.sleep(0.1)
+
+def lights():
+	white()
+
 
 def turn_x(x):
 	pub = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size = 10)
@@ -74,15 +99,28 @@ def check():
 		print("Teleop ended.")
 		exit(0)
 
-def face_cb(msg):
+def touch_cb(msg):
+	if msg:
+		#print(msg)
+		for i in range(len(msg.electrodes)):
+			if(msg.electrodes[i]):
+				print("I am being touched")
+				happy()
+			else:
+				white()
+
+
+def face_cb(msg): #face sensor callback
 	#if msg:
 	
+
 	helo = "potato"
 	if msg.faces.faces:
-		print(msg.faces.faces[0].center)
+		#print(msg.faces.faces[0].center)
 		print("I see a face!")
 		turn_x(msg.faces.faces[0].center.x)
-		#light_up(.3, .3, .3, 0, 2, 4)
+		#rainbow(.3, .3, .3, 0, 2, 4)
+		hello = "hi"
 	 	
 	# 	#print(faces)
 	# 	print(frame)
@@ -92,11 +130,20 @@ def run():
 	#vc.activate_module(module_name=VisionClient.FACE_DETECTOR)
 	#fr = FrameResults()
 	node = rospy.init_node('potato')
-	vs = rospy.Subscriber(
+
+	vs = rospy.Subscriber(#subscriber for vision sensor
 		"vision/results",
 		FrameResults,
 		face_cb
 	)
+
+
+	ts = rospy.Subscriber(#subscriber for touch sensors
+		"/mobile_base/touch", 
+		Touch, 
+		touch_cb
+	)
+
 	print("face detection program")
 	print(FrameResults)
 	rospy.spin()
